a0aab52df8accdac1651210e68488879f0378191
Asif Sheikh <sheikh.asif@gmail.com>
2016-01-09 11:56:30 -0800

    Added support for searching by multiple tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,35 @@
 class TagsController < ApplicationController
   def show
-    @tag = Tag.where(:name => params["name"]).first
-    @data = @tag.data.all if @tag else []
+    string_tags = (params["tags"] || "").split(/[\s,+]+/)
+    @tags = string_tags.map { |s| Tag.where(:name => s).first }.compact
+
+    if @tags.count == 0
+      redirect_to data_path
+    else
+      first = @tags.first
+      @data = first.data.all
+      # TODO: Figure out if we can do a table join instead.
+      @tags[1..-1].each { |t|
+        @data = @data.select { |d| d.tags.include?(t) }
+      }
+      @grouped_data = group_by_day(@data)
+    end
+  end
+
+private
+  # Groups the data by date, and computes the average value per day
+  def group_by_day(array)
+    dmap = {}
+    array.each { |d|
+      sdate = d.created_at.strftime("%Y-%m-%d")
+      dmap[sdate] = [] if dmap[sdate].nil?
+      dmap[sdate] << d.value
+    }
+
+    dmap.keys.each { |date|
+      avg = dmap[date].inject{ |sum, el| sum + el }.to_f / dmap[date].size
+      dmap[date] = avg
+    }
+    return dmap
   end
 end
